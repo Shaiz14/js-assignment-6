2fa70616c24e8edc569afe5340c4834e78f58b03
Shaiz14 <talhafreelance14@gmail.com>
2023-09-13 18:16:56 +0500

    add files

---
+++ app.js
@@ -0,0 +1,32 @@
+
+// Q NO 1
+
+// function date() {
+//  var currentDate = new Date();
+    //   console.log(currentDate);
+    // }
+    
+    // date();
+    
+    // Q No 2
+    
+    // function greet(firstName, lastName) {
+    //   var greeting = firstName + " "+lastName;
+    //   return greeting;
+    // }
+    // console.log(
+    //   greet(prompt("Enter your first name"), prompt("Enter your last Name"))
+    // );
+    
+//    Q NO 3
+    
+    function calc(firstValue, secondValue) {
+      var sum = firstValue + secondValue;
+      return sum;
+    }
+    
+    console.log(
+      calc(+prompt("Please enter a number"), +prompt("Sum with anther number"))
+    );
+    
+                                                
